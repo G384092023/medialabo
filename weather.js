54494b6458128c2da004cfb87522e3dc70b3e23d
G384092023 <238409@st.takushoku-u.ac.jp>
2024-05-30 12:29:16 +0900

    pulldown

--- weather.js
+++ weather.js
@@ -119,15 +119,24 @@ const tbl = document.createElement("table");
   tbl.insertAdjacentElement('afterend', p);
   
   
-  let b = document.querySelector('#print');
-  b.addEventListener('click', greeting);
   
-  // 2. イベントハンドラの定義
-  function greeting() {
-      let i = document.querySelector('input[name="city"]');
-      let city = i.value;       // ユーザが記入した文字列
-      console.log('都市:' + city );
-  }
+  
+  
+
+  let b = document.querySelector('button#btn');
+b.addEventListener('click', showSelectResult);
+
+function showSelectResult() {
+    let s = document.querySelector('select#place');
+    let idx = s.selectedIndex;  // idx 番目の option が選択された
+
+    let os = s.querySelectorAll('option');  // s の子要素 option をすべて検索
+    let o = os.item(idx);       // os の idx 番目の要素
+
+    console.log('選択された ' + idx + ' 番目の option の情報:');
+    console.log('  value=' + o.getAttribute('value'));  // id 属性を表示
+    console.log('  textContent='+o.textContent);
+}
  
 
   
